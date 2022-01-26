41200574609fe454d6d8b59238c9ada65ccb3336
jungle <dgjungle_c@163.com>
2022-01-26 22:45:19 +0800

    perfect the html2md

--- unit1/scrape_url/src/main.rs
+++ unit1/scrape_url/src/main.rs
@@ -1,14 +1,22 @@
-use std::fs;
 fn main() {
-    let url = "http://www.rust.org/";
-    let output = "rust.md";
-
+    for arg in std::env::args() {
+        println!("{}", arg);
+    }
+    let args: Vec<String> = std::env::args().collect();
+    // let url = "http://www.rust.org/";
+    // let output = "rust.md";
+    if args.len() <= 2 {
+        println!("Sorry please input the url and output");
+        return;
+    }
+    let url = &args[1];
+    let output = &args[2];
     println!("Fecthing url :{}", url);
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    std::fs::write(output, md.as_bytes()).unwrap();
 
     println!("Converted markdown has been saved in {}", output);
 }
