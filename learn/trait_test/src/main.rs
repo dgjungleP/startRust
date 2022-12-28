c8b9a97bd1fb56d3f75861b2fddfca9fb2d8be60
Jungle <dgJungle_c@163.com>
2022-12-28 22:01:26 +0800

    learn rust

--- learn/trait_test/src/main.rs
+++ learn/trait_test/src/main.rs
@@ -1,7 +1,7 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
 
-    for &item in list {
+    for item in list {
         if item > largest {
             largest = item;
         }
@@ -10,39 +10,76 @@ fn largest_i32(list: &[i32]) -> i32 {
     largest
 }
 
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
+use std::fmt::Display;
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
-    assert_eq!(result, 100);
+    assert_eq!(*result, 100);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
-    assert_eq!(result, 'y');
+    assert_eq!(*result, 'y');
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of caurse, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet:{}", tweet.summarize());
+    notify(&tweet);
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("(Reading more from {}...)", self.summarize_author())
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news!{}", item.summarize());
+}
+pub fn notify2<T: Summary + Display>(item: &T) {
+    println!("Breaking news!{}", item.summarize());
+}
+pub fn notify3<T>(item: &T)
+where
+    T: Summary + Display,
+{
+    println!("Breaking news!{}", item.summarize());
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
 }
